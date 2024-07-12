305890277ee29fa93834b02bb63ad0c8665f6ed3
PradeepKumarReddyEda <pedara@launchcg.com>
2024-07-12 17:50:25 +0530

    First Test Case

--- pythonlearning/firsttestcase.py
+++ pythonlearning/firsttestcase.py
@@ -1,8 +1,9 @@
+ 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 # Use a raw string or double backslashes for the path
-chromedriver_path = r"D:\Python\drivers\chrome-win64\chrome-win64\chromedriver"
+chromedriver_path = r"C:\drivers\chromedriver-win64\chromedriver-win64\chromedriver.exe"
 
 # Specify the path to the WebDriver executable
 service = Service(executable_path=chromedriver_path)
@@ -10,5 +11,11 @@ service = Service(executable_path=chromedriver_path)
 # Initialize the WebDriver with the Service object
 driver = webdriver.Chrome(service=service)
 
-# Your test case code here
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+driver.find_element_by_name()
+
+
+
+
 
