79f53e73ca2bf1cb94a3c48843b6aff3af7799d4
PradeepKumarReddyEda <pedara@launchcg.com>
2024-07-12 19:07:34 +0530

    First Test Case

---
+++ pythonlearning/FirstTestCase.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+time.sleep(2)
+
+driver.find_element(By.NAME,"username").send_keys("Admin")
+driver.find_element(By.NAME,"password").send_keys("admin123")
+driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+
+time.sleep(2)
+
+act_title=driver.title
+exp_title="OrangeHRM"
+if act_title==exp_title:
+    print("Login Test Passed")
+else:
+    print("Login Test Failed")
+driver.close()
