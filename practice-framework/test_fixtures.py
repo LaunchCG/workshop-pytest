4a7bf362549cad0946326d0f6b703b40429b8eec
DKarmakar <dkarmakar@launchcg.com>
2024-07-18 14:27:41 +0530

    initial setup

---
+++ practice-framework/test_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+class TestClass:
+    def test_Login(self):
+        print("this is login test")
+    def test_Search(self):
+        print("this is search test")
+    def test_Advancedsearch(self):
+        print("this is advanced search test")
