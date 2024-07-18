4114ece33c3bcc879defcef043d547107b804b65
DKarmakar <dkarmakar@launchcg.com>
2024-07-18 14:45:40 +0530

    initial setup

--- practice-framework/test_fixtures.py
+++ practice-framework/test_fixtures.py
@@ -1,8 +1,15 @@
 import pytest
 class TestClass:
+    @pytest.fixture() # decorator
+    def setup(self):
+        print("launching browser...")
+        print("Open application.")
+
     def test_Login(self):
         print("this is login test")
-    def test_Search(self):
+
+    def test_Search(self,setup):
         print("this is search test")
-    def test_Advancedsearch(self):
+
+    def test_Advancedsearch(self,setup):
         print("this is advanced search test")
