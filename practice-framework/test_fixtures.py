a60b07773e60b8f669b2aa0934c5f6da4b4253d7
DKarmakar <dkarmakar@launchcg.com>
2024-07-18 14:58:20 +0530

    initial setup

--- practice-framework/test_fixtures.py
+++ practice-framework/test_fixtures.py
@@ -3,9 +3,11 @@ class TestClass:
     @pytest.fixture() # decorator
     def setup(self):
         print("launching browser...")
-        print("Open application.")
+        print("Open application.")          #executes once before each test method
+        yield
+        print("close browser")              #executes once after each test method
 
-    def test_Login(self):
+    def test_Login(self, setup):
         print("this is login test")
 
     def test_Search(self,setup):
