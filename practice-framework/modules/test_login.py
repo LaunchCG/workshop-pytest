03f84526aba6d85a76c36b35eadd989c3c5f6c55
DKarmakar <dkarmakar@launchcg.com>
2024-07-18 18:55:52 +0530

    initial setup

--- practice-framework/modules/test_login.py
+++ practice-framework/modules/test_login.py
@@ -1,13 +1,17 @@
-  #test_login.py
+# test_login.py
 
 import pytest
+
+
 class TestLogin:
-    def test_loginbyemail(self,setup):
+    def test_loginbyemail(self, setup):
         print("this is login by email")
         assert True == True
-    def test_loginbyfacebook(self,setup):
+
+    def test_loginbyfacebook(self, setup):
         print("this is login by facebook")
         assert True == True
-    def test_loginbytwitter(self,setup):
+
+    def test_loginbytwitter(self, setup):
         print("this is login by twitter")
         assert True == True
