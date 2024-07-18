6e11f8f5102ccf3d97cb19f3e4f153870e74dbd8
Siva Kumar Anbalagan <SAnbalagan@launchcg.com>
2024-07-18 15:23:33 +0530

    Simple tests - Understanding grouping, verbose and keywords when running a test

---
+++ SimpleTests/test_FirstProgram.py
@@ -0,0 +1,11 @@
+class TestClass:
+    def testlogin(self):
+        print("Login successful !!")
+
+    def testlogoff(self):
+        print("Logoff successful !!")
+
+    def testcalculation(self):
+        assert 2+3 == 5
+
+
