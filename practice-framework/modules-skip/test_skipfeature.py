ef2d30214aeec4515718597d335f8e4105a430a3
DKarmakar <dkarmakar@launchcg.com>
2024-07-18 19:09:02 +0530

    initial setup

---
+++ practice-framework/modules-skip/test_skipfeature.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+class TestClass:
+    def test_loginbyemail(self):
+        print("this is login by email")
+        assert 1 == 1
+
+    def test_loginbyfacebook(self):
+        print("this is login by facebook")
+        assert 1 == 1
+
+    def test_loginbytwitter(self):
+        print("this is login by twitter")
+        assert 1 == 1
+
+    def test_signupbyemail(self):
+        print("this is signup by email")
+        assert 1 == 1
+
+    def test_signupbyfacebook(self):
+        print("this is signup by facebook")
+        assert 1 == 1
+
+    def test_signupbytwitter(self):
+        print("this is signup by twitter")
+        assert 1 == 1
