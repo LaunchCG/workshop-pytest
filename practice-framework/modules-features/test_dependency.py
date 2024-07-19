863df1b08dc76ddc822490eb359b7fc0731c765a
DKarmakar <dkarmakar@launchcg.com>
2024-07-19 10:34:02 +0530

    initial setup

---
+++ practice-framework/modules-features/test_dependency.py
@@ -0,0 +1,21 @@
+import pytest
+
+class TestClass:
+    @pytest.mark.dependency()
+    def test_openapp(self):
+        assert True
+    @pytest.mark.dependency(depends=["TestClass::test_openapp"])
+    def test_login(self):
+        assert True
+
+    @pytest.mark.dependency(depends=["TestClass::test_login"])
+    def test_search(self):
+        assert True
+
+    @pytest.mark.dependency(depends=["TestClass::test_login","TestClass::test_search"])
+    def test_advsearch(self):
+        assert True
+
+    @pytest.mark.dependency(depends=["TestClass::test_login"])
+    def test_logout(self):
+        assert True
