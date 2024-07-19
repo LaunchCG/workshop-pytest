8b834c8da90cd821b894411eb66d5093cf9e4a9f
DKarmakar <dkarmakar@launchcg.com>
2024-07-19 09:58:43 +0530

    initial setup

--- practice-framework/modules-features/test_skipfeature.py
+++ practice-framework/modules-features/test_skipfeature.py
@@ -6,6 +6,7 @@ class TestClass:
         print("this is login by email")
         assert 1 == 1
 
+    @pytest.mark.skip
     def test_loginbyfacebook(self):
         print("this is login by facebook")
         assert 1 == 1
@@ -14,6 +15,7 @@ class TestClass:
         print("this is login by twitter")
         assert 1 == 1
 
+    @pytest.mark.skip
     def test_signupbyemail(self):
         print("this is signup by email")
         assert 1 == 1
