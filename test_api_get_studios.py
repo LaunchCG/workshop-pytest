71aea6ba45450fa4d0731226b2fd49a472b8dc34
PradeepKumarReddyEda <pedara@launchcg.com>
2024-07-24 22:10:56 +0530

    Working with APIs

--- test_api_get_studios.py
+++ test_api_get_studios.py
@@ -1,8 +1,12 @@
 import requests
 
-def test_get():
-    url = 'http://127.0.0.1:8000/'
+def test_post():
+    url = 'http://127.0.0.1:8000/studios/post'
     response = requests.get(url)
     print(response.status_code)
     print(response.json())
     assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+
+    data = response.json()
+    assert data["studio_name_created"]== "Another cool studio1"
